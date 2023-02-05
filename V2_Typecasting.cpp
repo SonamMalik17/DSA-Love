302ee6bf601ff9e7360eafd6272fec4158b1f9dc
SonamMalik17 <maliksonam0301@gmail.com>
2023-02-05 23:09:06 +0530

    Typecasting

---
+++ V2_Typecasting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 'a';
+    cout<<"a="<<a<<endl;
+
+    char ch1=98;
+    cout<<"ch1="<<ch1<<endl;
+
+    char ch2=123456;
+    cout<<"ch2="<<ch2<<endl;
+
+    return 0;
+}
