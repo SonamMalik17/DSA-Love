e9d9d271bd20d903dd1d92c436601ced5d16aed3
SonamMalik17 <maliksonam0301@gmail.com>
2023-02-08 14:17:52 +0530

    UnsignedInt

---
+++ UnsignedInt.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    unsigned int a = 2;
+    cout<<"unsigned int a = "<<a<<endl<<endl;
+    // Trying to store negative numbers in unsigned int 
+    unsigned int b = -2;
+    cout<<"unsigned int b = "<<b<<endl<<endl;
+    return 0;
+}
