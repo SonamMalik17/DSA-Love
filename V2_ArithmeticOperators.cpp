3c6e079898c6f5cb8c394ac2dea85c6b6bd32cbb
SonamMalik17 <maliksonam0301@gmail.com>
2023-02-08 15:15:50 +0530

    ArithmeticOperators

---
+++ V2_ArithmeticOperators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 2/5;
+    cout<<"a = "<<a<<endl<<endl;
+
+    float b = 2/5;
+    cout<<"b = "<<b<<endl<<endl;
+
+    int c = 2.0/5;
+    cout<<"c = "<<c<<endl<<endl; 
+
+    float d = 2.0/5;
+    cout<<"d = "<<d<<endl<<endl; 
+
+    cout<<"2/5 = "<<2/5<<endl<<endl;
+
+    cout<<"2.0/5 = "<<2.0/5<<endl<<endl;
+
+    return 0;
+}
