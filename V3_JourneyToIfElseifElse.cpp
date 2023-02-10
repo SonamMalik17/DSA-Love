eab32f6001e4f49d3b7d235c63cd3dce1f41842c
SonamMalik17 <maliksonam0301@gmail.com>
2023-02-10 20:39:21 +0530

    Questions Added - Journey to If ElseIf Else

--- V3_JourneyToIfElseifElse.cpp
+++ V3_JourneyToIfElseifElse.cpp
@@ -51,7 +51,7 @@ int main()
 
 
 
-// Better Version of the program above 
+    // Better Version of the program above 
     int d;
     cout<<"Enter the value of d : ";
     cin>>d;
@@ -67,4 +67,66 @@ int main()
     {
         cout<<"d is zero"<<endl<<endl;
     }
+
+
+
+    // Q1
+    int r=9;
+    if(r==9){
+        cout<<"NINEY";
+    }
+    if(r>0){
+        cout<<"POSITIVE";
+    }
+    else{
+        cout<<"NEGATIVE";
+    }
+
+
+
+    // Q2
+    int s=2;
+    int t=s+1;
+    if((s=3)==t){
+        // true and printed
+        cout<<s;
+    }
+    else{
+        cout<<s+1;
+    }
+
+
+
+    // Q3
+    int z=24;
+    if(z>20){
+        cout<<"Love ";
+    }
+    else if(z==24){
+        cout<<"Lovely";
+    }
+    else{
+        cout<<"Babbar";
+    }
+    cout<<z;
+
+
+
+    // Q4 (H/W)
+    char ch;
+    cout<<"Enter a character : ";
+    cin>>ch;
+    if(ch>='a' && ch<='z'){
+        cout<<"This is Lowercase";
+    }
+    else if(ch>='A' && ch<='Z'){
+        cout<<"This is Uppercase";
+    }
+    else if(ch>='0' && ch<='9'){
+        cout<<"This is Numeric";
+    }
+
+
+    
+    return 0;
 }
