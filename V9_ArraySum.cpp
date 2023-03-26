ab2b8f48465ce453f26859b0f0746a313c5b10e7
SonamMalik17 <maliksonam0301@gmail.com>
2023-03-26 17:07:14 +0530

    Array Sum

---
+++ V9_ArraySum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int arraySum(int a[],int n)
+{
+    int sum=0;
+    for(int i=0;i<n;i++)
+        sum+=a[i];
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of array : "<<endl;
+    cin>>n;
+    int a[100];
+    cout<<"Enter array elements : "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    int sum=arraySum(a,n);
+    cout<<"Sum : "<<sum<<endl;
+    return 0;
+}
